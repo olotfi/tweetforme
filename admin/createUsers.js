a4e3762bb8bbb8d71045b826d58408dfc53c1ac8
Omar Lotfi <o@caldera.vc>
2022-02-06 17:43:55 +1100

    Add script to create Firebase Auth users with claims

---
+++ admin/createUsers.js
@@ -0,0 +1,24 @@
+const { initializeApp, applicationDefault } = require('firebase-admin/app');
+const { getAuth } = require('firebase-admin/auth');
+const users = require('./a-users.json');
+
+// Initialise Firebase
+initializeApp({
+  credential: applicationDefault() // Needs GOOGLE_APPLICATION_CREDENTIALS env var set to the service account key JSON file
+});
+
+// Load users and create them
+for (const user of users) {
+  createUser(user);
+}
+
+async function createUser(data) {
+  const auth = getAuth();
+  const userRecord = await auth.createUser({
+    email: data.email,
+    password: data.password,
+    displayName: data.name
+  });
+  await auth.setCustomUserClaims(userRecord.uid, { roles: data.roles });
+  console.log('Successfully created new user:', userRecord.uid, userRecord.email);
+}
